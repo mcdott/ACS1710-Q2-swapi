daf937ec96a466906300f22c9b604a604b9ccf82
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-21 23:58:39 -0500

    Added results route and results page

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +8,21 @@ app = Flask(__name__)
 def search():
     return render_template('character-search-form.html')
 
+@app.route('/results')
+def results():
+    swapi_url = 'https://swapi.py4e.com/api/'
+    character_id = request.args.get('character_id')
+    character_response = requests.get(swapi_url + 'people/' + character_id)
+    character_result = json.loads(character_response.content)
+    # >>>>>>>
+    print(character_id)
+    print(character_result)
 
+
+    context = {
+        'character': character_result
+        }
+    return render_template('results.html', **context)
 
 
 if __name__ == '__main__':
